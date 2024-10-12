56a605a3c15f96848c7154acfd27d88ea3f3fd8b
maliha-afk <24amaliha@gmail.com>
2024-10-12 20:24:25 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+class dog:
+    def __init__(self, breed,colour):
+        self.breed=breed
+        self.colour=colour
+Alaskan_Malamute=dog("domesticated wolf-dogs","Grey, sable, black, or red, always with white, as well as all white and brown")
+Siberian_Husky=dog("Chukotka Sled Dog","All colors from black to pure white, and including many differing colors and markings")
+
+
+print(Alaskan_Malamute.breed)
+
+print(Alaskan_Malamute.colour)
+
+print(Siberian_Husky.breed)
+
+print(Siberian_Husky.colour)
